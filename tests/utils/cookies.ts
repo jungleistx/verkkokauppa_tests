7b74333d7fda132ac9b879c5c6240555ede01d06
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-02-17 22:36:29 +0200

    add waitFor in cookies.ts

--- tests/utils/cookies.ts
+++ tests/utils/cookies.ts
@@ -4,7 +4,10 @@ import { Page } from '@playwright/test';
 export async function acceptCookiesIfVisible(page: Page) {
 	const cookieButton = page.getByRole('button', { name: 'Vain välttämättömät' });
 
-	if (await cookieButton.isVisible()) {
+	try {
+		await cookieButton.waitFor({ state: 'visible', timeout: 5000 });
 		await cookieButton.click();
+	} catch {
+		// no popup, continue
 	}
 };
