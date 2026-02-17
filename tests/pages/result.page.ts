93748add08f70138e845a16a2af70020fa7e9a31
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-02-18 01:51:00 +0200

    add assertions in sortings in result.page.ts

--- tests/pages/result.page.ts
+++ tests/pages/result.page.ts
@@ -1,4 +1,4 @@
-import { Page } from '@playwright/test';
+import { expect, Page } from '@playwright/test';
 
 
 export class ResultPage {
@@ -14,29 +14,36 @@ export class ResultPage {
 
 	async selectResultByIndex(n: number) {
 		await this.page.locator('#main ol > li').nth(n).click();
+		await expect(this.page).toHaveURL(/product/);
 	}
 
 	async sortResultsPriceAsc() {
 		await this.sortDropdown.selectOption('price:asc');
+		await expect(this.page).toHaveURL(/price.*asc/);
 	}
 
 	async sortResultsPriceDesc() {
 		await this.sortDropdown.selectOption('price:desc');
+		await expect(this.page).toHaveURL(/price.*desc/);
 	}
 
 	async sortResultsByDate() {
 		await this.sortDropdown.selectOption('releaseDate:desc');
+		await expect(this.page).toHaveURL(/releaseDate.*desc/);
 	}
 
 	async sortResultsMostSold() {
 		await this.sortDropdown.selectOption('popularity:desc');
+		await expect(this.page).toHaveURL(/popularity.*desc/);
 	}
 
 	async sortResultsByRating() {
 		await this.sortDropdown.selectOption('rating:desc');
+		await expect(this.page).toHaveURL(/rating.*desc/);
 	}
 
 	async sortResultsByScore() {
 		await this.sortDropdown.selectOption('score:desc');
+		await expect(this.page).toHaveURL(/score.*desc/);
 	}
 };
