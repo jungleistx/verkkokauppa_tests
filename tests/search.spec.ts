ec6c69f1cd4c87854bf2dca73d0f0019be94f557
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-02-18 15:21:41 +0200

    rename example.ts to seach.spec.ts, add test search, sort and select 2nd

---
+++ tests/search.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+import { SearchComponent } from './pages/search.component';
+import { acceptCookiesIfVisible } from './utils/cookies';
+import { ResultPage } from './pages/result.page';
+
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/');
+  await acceptCookiesIfVisible(page);
+});
+
+
+test('search for nikon, sort by price and select 2nd', async ({ page }) => {
+  const search = new SearchComponent(page);
+  const results = new ResultPage(page);
+
+  await search.searchForItem('Nikon');
+  await results.sortResultsPriceDesc();
+  await results.selectResultByIndex(1);
+
+  await expect(page).toHaveTitle(/nikon/i);
+  // // task 1 version, add soft assertion
+  // await expect(page).toHaveTitle(/Nikon Z30/);
+});
