349e19d2f76982fb136e11bdcdbc8cb4a7e87f1a
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-02-18 02:00:14 +0200

    add waitForLoadState in result.page, wait for sorting to load

--- tests/pages/result.page.ts
+++ tests/pages/result.page.ts
@@ -19,31 +19,37 @@ export class ResultPage {
 
 	async sortResultsPriceAsc() {
 		await this.sortDropdown.selectOption('price:asc');
+		await this.page.waitForLoadState();
 		await expect(this.page).toHaveURL(/price.*asc/);
 	}
 
 	async sortResultsPriceDesc() {
 		await this.sortDropdown.selectOption('price:desc');
+		await this.page.waitForLoadState();
 		await expect(this.page).toHaveURL(/price.*desc/);
 	}
 
 	async sortResultsByDate() {
 		await this.sortDropdown.selectOption('releaseDate:desc');
+		await this.page.waitForLoadState();
 		await expect(this.page).toHaveURL(/releaseDate.*desc/);
 	}
 
 	async sortResultsMostSold() {
 		await this.sortDropdown.selectOption('popularity:desc');
+		await this.page.waitForLoadState();
 		await expect(this.page).toHaveURL(/popularity.*desc/);
 	}
 
 	async sortResultsByRating() {
 		await this.sortDropdown.selectOption('rating:desc');
+		await this.page.waitForLoadState();
 		await expect(this.page).toHaveURL(/rating.*desc/);
 	}
 
 	async sortResultsByScore() {
 		await this.sortDropdown.selectOption('score:desc');
+		await this.page.waitForLoadState();
 		await expect(this.page).toHaveURL(/score.*desc/);
 	}
 };
