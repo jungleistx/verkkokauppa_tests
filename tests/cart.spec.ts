fdaa3f07f52fa8699eff4f3b88decc56000b4d17
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-02-19 15:06:28 +0200

    add test remove items from cart to cart.spec

--- tests/cart.spec.ts
+++ tests/cart.spec.ts
@@ -30,3 +30,21 @@ test('verify empty cart, add item, verify full cart', async ({ page }) => {
   await cart.goto();
   await cart.assertCartNotEmpty();
 });
+
+
+test('add 2 items, remove, verify empty cart', async ({ page }) => {
+  const cart = new CartPage(page);
+
+  await cart.goto();
+  await cart.assertCartIsEmpty();
+
+  await page.goto('/product/343139/')
+  await cart.addItemToCartFromProductPage();
+
+  await page.goto('/product/419537/')
+  await cart.addItemToCartFromProductPage();
+
+  await cart.goto();
+  await cart.removeAllItemsFromCart();
+  await cart.assertCartIsEmpty();
+});
