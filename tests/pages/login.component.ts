7d5d9308419cf4aeee48d0104f9f82f63576e252
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-02-19 23:17:43 +0200

    add loginWithArgCredentials to login.component

--- tests/pages/login.component.ts
+++ tests/pages/login.component.ts
@@ -60,6 +60,22 @@ export class LoginComponent {
 		await this.greetingText.waitFor({ state: 'visible', timeout: 5000 });
 	}
 
+	async loginWithArgCredentials(username: string, password: string) {
+		await this.openLoginMenu();
+
+		await this.emailTextfield.waitFor({ state: 'visible' });
+		await this.passwordTextfield.waitFor({ state: 'visible' });
+
+		await this.emailTextfield.fill(username);
+		await this.passwordTextfield.fill(password);
+
+		await this.loginButton.click();
+
+		await this.greetingText.waitFor({ state: 'visible' , timeout: 5000 });
+		await this.page.reload();
+		await this.greetingText.waitFor({ state: 'visible', timeout: 5000 });
+	}
+
 	async logoutUser() {
 		await this.greetingText.waitFor({ state: 'visible' });
 
